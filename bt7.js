cdf9d51fe056e642a12cb2e360198c8f1cbac9a5
phongpheu12101991 <62396380+phongpheu12101991@users.noreply.github.com>
2020-05-13 18:53:00 +0700

    Add files via upload

---
+++ bt7.js
@@ -0,0 +1,194 @@
+// C/R/U/D - phone list
+// Cho user input C/R/U/D
+// C - name,phone -> them vao mang 
+//    => R  
+// R - in ra toan bo contact theo name-phone
+// U - cho user tim contact theo name
+//     ko thay thi not found
+//     thay thi update ten va sdt moi
+//     (ten moi ko trung ten cu)
+//    => R
+// D - cho user tim contact theo name
+//     ko thay thi not found
+//     thay thi xoa contact
+//    => R
+// ****Neu co roi ko tao moi
+// ****Tim ten rut gon
+
+const phonelist = [
+    {
+        name: 'phong',
+        phone: '01658214578',
+    },
+    {
+        name: 'phu',
+        phone: '01010203232',
+    }
+];
+
+function Lastname() {
+    let LastNameArray = [];
+    for (contact of phonelist) {
+        x = contact.name.split(' ');
+        LastNameArray.push(x[x.length - 1]);
+    } return LastNameArray;
+};
+function findname(arrinput) {
+    let LastNameArray = [];
+    for (contact of phonelist) {
+        x = contact.name.split(' ');
+        LastNameArray.push(x[x.length - 1]);
+    }
+    InputtoArray = arrinput.split(' ');
+    LastNameInput = InputtoArray[InputtoArray.length - 1];
+    for (let i = 0; i < LastNameArray.length; i++) {
+        if (LastNameArray[i] == LastNameInput) {
+            find = LastNameArray[i];
+            break;
+        } else {
+            find = false;
+        }
+    } return find;
+};
+
+
+
+function showall(arr) {
+    for (contact of arr) {
+        console.log(contact.name + ' - ' + contact.phone);
+    };
+};
+while (true) {
+    let ui = prompt('Nhap thao tac can lam C/R/U/D ?').toUpperCase();
+    if (ui == 'R') {
+        showall(phonelist);
+    }
+    else if (ui == 'C') {
+        let newname = prompt('Nhap ten lien lac moi');
+        findname(newname);
+        if (find == false) {
+            let newphone = prompt('Nhap so');
+            phonelist.push({ name: newname, phone: newphone });
+            showall(phonelist);
+        } else {
+            alert('Ten nay da co roi');
+        };
+
+
+    }
+    else if (ui == 'U') {
+        let uiu = prompt('Ten lien lac can sua?');
+        findname(uiu);
+        let FixIndex = Lastname().indexOf(find);
+        if (find == false) {
+            alert('Khong tim thay ten');
+        } else {
+            let newname1 = prompt('Nhap ten moi');
+            findname(newname1);
+            if (find == false) {
+                let newphone1 = prompt('Nhap so moi');
+                phonelist[FixIndex].name = newname1;
+                phonelist[FixIndex].phone = newphone1;
+                showall(phonelist);
+            } else {
+                alert('Ten nay da co roi');
+            }
+        };
+    }
+    else if (ui == 'D') {
+        let uid = prompt('Ten lien lac muon xoa?');
+        findname(uid);
+        let DeleteIndex = Lastname().indexOf(find);
+        if (find == false) {
+            alert('Khong tim thay ten');
+        } else {
+            phonelist.splice(DeleteIndex, 1);
+            showall(phonelist);
+        };
+    } else {
+        alert('Ban nhap sai cau lenh');
+    };
+};
+
+
+// *** Phần dưới này bị lỗi function không check được tên đầu tiên
+// *** Giải thích hộ mình với
+
+// function findname(arrinput) {
+//     let LastNameArray = [];
+//     for (contact of phonelist) {
+//         x = contact.name.split(' ');
+//         LastNameArray.push(x[x.length - 1]);
+//     }
+//     InputtoArray = arrinput.split(' ');
+//     LastNameInput = InputtoArray[InputtoArray.length - 1];
+//     for (let i = 0; i < LastNameArray.length; i++) {
+//         if (LastNameArray[i] == LastNameInput) {
+//             find = i;
+//             break;
+//         } else {
+//             find = false;
+//         }
+//     } return find;
+// };
+
+
+
+// function showall(arr) {
+//     for (contact of arr) {
+//         console.log(contact.name + ' - ' + contact.phone);
+//     };
+// };
+// while (true) {
+//     let ui = prompt('Nhap thao tac can lam C/R/U/D ?').toUpperCase();
+//     if (ui == 'R') {
+//         showall(phonelist);
+//     }
+//     else if (ui == 'C') {
+//         let newname = prompt('Nhap ten lien lac moi');
+//         findname(newname);
+//         if (find == false) {
+//             let newphone = prompt('Nhap so');
+//             phonelist.push({ name: newname, phone: newphone });
+//             showall(phonelist);
+//         } else {
+//             alert('Ten nay da co roi');
+//         };
+
+
+//     }
+//     else if (ui == 'U') {
+//         let uiu = prompt('Ten lien lac can sua?');
+//         findname(uiu);
+//         let FixIndex = find;
+//         if (find == false) {
+//             alert('Khong tim thay ten');
+//         } else {
+//             let newname1 = prompt('Nhap ten moi');
+//             findname(newname1);
+//             if (find == false) {
+//                 let newphone1 = prompt('Nhap so moi');
+//                 phonelist[FixIndex].name = newname1;
+//                 phonelist[FixIndex].phone = newphone1;
+//                 showall(phonelist);
+//             } else {
+//                 alert('Ten nay da co roi');
+//             }
+//         };
+//     }
+//     else if (ui == 'D') {
+//         let uid = prompt('Ten lien lac muon xoa?');
+//         findname(uid);
+//         let DeleteIndex = find;
+//         if (find == false) {
+//             alert('Khong tim thay ten');
+//         } else {
+//             phonelist.splice(DeleteIndex, 1);
+//             showall(phonelist);
+//         };
+//     } else {
+//         alert('Ban nhap sai cau lenh');
+//     };
+// };
+
+
